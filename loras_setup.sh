44c6fb66aeb12db6f44dd4e8da3b763fbf1604b0
Jason Hardin <jason@jasonhardin.com>
2023-11-07 19:25:31 -0500

    Update loras_setup.sh

--- loras_setup.sh
+++ loras_setup.sh
@@ -1,4 +1,4 @@
 # Add Loras
-cd "${comfyui_dir}/loras"
+cd "${comfyui_dir}/models/loras"
 # DnD Lora https://civitai.com/models/134343/dungeons-and-dragons-style-fantasy-lora-xl?modelVersionId=152042
 curl -L https://civitai.com/api/download/models/152042 --output dungeons_and_dragons.safetensors &
