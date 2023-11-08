34ffb488172303234de38cb19ac12e9f5230c0c5
Jason Hardin <jason@jasonhardin.com>
2023-11-07 21:11:55 -0500

    all in one script is back.

--- runpod_comfyui_setup.sh
+++ runpod_comfyui_setup.sh
@@ -4,13 +4,13 @@ export comfyui_dir
 export main_dir
 
 apt-get update
+source "${comfyui_dir}/venv/bin/activate"
 
 bash comfyui_setup.sh
 bash loras_setup.sh
 bash vae_setup.sh
 bash upscale_models_setup.sh
 
-source "{$comfyui_dir}/venv/bin/activate"
 bash ultimate_flow_setup.sh
 bash seargesdxl_install.sh
 
