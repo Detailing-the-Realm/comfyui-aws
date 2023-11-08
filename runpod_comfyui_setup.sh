787cd6828c4106171721b7cc3e9216872037d84d
Jason Hardin <jason@jasonhardin.com>
2023-11-07 19:31:15 -0500

    Update runpod_comfyui_setup.sh

--- runpod_comfyui_setup.sh
+++ runpod_comfyui_setup.sh
@@ -8,6 +8,10 @@ apt-get update
 bash comfyui_setup.sh
 bash loras_setup.sh
 bash vae_setup.sh
+bash upscale_models_setup.sh
+
 source "{$comfyui_dir}/venv/bin/activate"
 bash ultimate_flow_setup.sh
 bash seargesdxl_install.sh
+
+bash checkpoints_setup.sh
